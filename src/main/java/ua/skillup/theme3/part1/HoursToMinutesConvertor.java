14778e4d19d1c9d7f4de82820075360154a1524d
Sasha2511 <nedzelnytskyis@gmail.com>
2024-04-12 19:39:18 +0300

    HoursToMinutesConvertor and SalaryCalculator resolving

--- src/main/java/ua/skillup/theme3/part1/HoursToMinutesConvertor.java
+++ src/main/java/ua/skillup/theme3/part1/HoursToMinutesConvertor.java
@@ -2,15 +2,21 @@ package ua.skillup.theme3.part1;
 
 public class HoursToMinutesConvertor {
     public static int convertHoursToMinutes(int hours) {
+        if (hours > 35791394 || hours < 0) {
+            throw new IllegalArgumentException("Hours can't be more than 35791394 and less than 0");
+        }
         return hours * 60;
     }
 
     public static int convertHoursToSeconds(int hours) {
+        if (hours > 596523 || hours < 0) {
+            throw new IllegalArgumentException("Hours can't be more than 596523 and less than 0");
+        }
         return hours * 60 * 60;
     }
 
     public static void main(String[] args) {
-        int hours = 1;
+        int hours = 35791399;
         int result = convertHoursToMinutes(hours);
         System.out.println(result);
         result = convertHoursToSeconds(hours);
