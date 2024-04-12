3ee79e71ec0179be3dc3330a1b85f50ab9f2739a
Sasha2511 <nedzelnytskyis@gmail.com>
2024-04-12 19:24:18 +0300

    DivisionWithRemainderCalculator resolving

--- src/main/java/ua/skillup/theme3/part1/DivisionWithRemainderCalculator.java
+++ src/main/java/ua/skillup/theme3/part1/DivisionWithRemainderCalculator.java
@@ -2,6 +2,10 @@ package ua.skillup.theme3.part1;
 
 public class DivisionWithRemainderCalculator {
     public static int calculateRemainder(int dividend, int divisor) {
+        if (divisor == 0) {
+            throw new IllegalArgumentException("The divisor is 0. Division by zero is prohibited");
+            //why can't the ArithmeticException be used?
+        }
         return dividend % divisor;
     }
 
