240aa7887262299a8dd6a4f92db8051633394096
Tetiana Yakymiuk <t882005@yahoo.com>
2024-04-20 23:22:55 -0400

    Some changes Hours To Minutes Convertor

--- src/main/java/ua/skillup/theme3/part1/HoursToMinutesConvertor.java
+++ src/main/java/ua/skillup/theme3/part1/HoursToMinutesConvertor.java
@@ -2,15 +2,21 @@ package ua.skillup.theme3.part1;
 
 public class HoursToMinutesConvertor {
     public static int convertHoursToMinutes(int hours) {
+        if (hours > 24 || hours < 0) {
+            throw new IllegalArgumentException("Hours can't be more than 24 and less 0");
+        }
         return hours * 60;
     }
 
     public static int convertHoursToSeconds(int hours) {
+        if (hours > 24 || hours < 0) {
+            throw new IllegalArgumentException("Hours can't be more than 24 and less 0");
+        }
         return hours * 60 * 60;
     }
 
     public static void main(String[] args) {
-        int hours = 1;
+        int hours = 25;
         int result = convertHoursToMinutes(hours);
         System.out.println(result);
         result = convertHoursToSeconds(hours);
